13a3c208b18327e92c6d3a86182dc465d0b1b0ba
Hemanth B <hemanth22hemu@gmail.com>
2019-02-09 20:39:28 +0530

    Create Dockerscript.sh

---
+++ Dockerscript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker network create elastic
+docker run -d --name elasticsearch --net elastic -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:6.6.0
+docker run -d --name kibana --net elastic -p 5601:5601 kibana:6.6.0
